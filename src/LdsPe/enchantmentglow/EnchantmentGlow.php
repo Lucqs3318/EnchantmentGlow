17e4114da43106f0ebf7567c74f7a6999b64769b
LdsPE <lucasdesouzalds2000@gmail.com>
2020-12-21 23:27:34 -0300

    Add files via upload

---
+++ src/LdsPe/enchantmentglow/EnchantmentGlow.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LdsPe\enchantmentglow;
+
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+
+class EnchantmentGlow{
+
+	public const GLOW = 255;
+
+	public static function register() : void
+	{
+		Enchantment::registerEnchantment(new Enchantment(self::GLOW, "glow", Enchantment::RARITY_COMMON, Enchantment::SLOT_ALL, Enchantment::SLOT_NONE, 1));
+	}
+
+	public static function addGlow(Item $item) : ?Item
+	{
+		$item->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantment(self::GLOW)));
+		return $item;
+	}
+
+	public static function removeGlow(Item $item) : ?Item
+	{
+		$item->removeEnchantment(self::GLOW);
+		return $item;
+	}
+}
